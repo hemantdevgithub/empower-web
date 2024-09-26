30a8e981214113401178821f99f8af432e62862c
parvez13913 <parvezz13913@gmail.com>
2024-09-26 19:03:00 +0600

    [DONE] Supply chain card

--- src/pages/teams/teams.tsx
+++ src/pages/teams/teams.tsx
@@ -1,11 +1,13 @@
 import BusinessDevelopmentCard from "./businessDevelopmentCard";
 import InvestorCommunityCard from "./investorCommunityCard";
+import SupplyChainCard from "./supplyChainCard";
 
 const Teams = () => {
   return (
     <div className="grid gap-[60px] lg:grid-cols-2">
       <InvestorCommunityCard />
       <BusinessDevelopmentCard />
+      <SupplyChainCard />
     </div>
   );
 };
