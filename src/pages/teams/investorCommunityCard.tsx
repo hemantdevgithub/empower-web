eac689dbe95cca8a11680b18325b75c117f90824
parvez13913 <parvezz13913@gmail.com>
2024-09-26 15:34:15 +0600

    [DONE] Investor Community card

--- src/pages/teams/investorCommunityCard.tsx
+++ src/pages/teams/investorCommunityCard.tsx
@@ -1,27 +1,28 @@
-import { ArrowRightIcon, GlobeIcon, PlusIcon } from "lucide-react";
+import { ArrowRightIcon, PlusIcon } from "lucide-react";
 import mainImage from "/image-2.png";
 import investor1 from "/Ellipse 171.png";
 import investor2 from "/Ellipse 172.png";
 import investor3 from "/Ellipse 174.png";
 import handshake from "/image-9.png";
+import GlobeIcon from "/Group 12023.png";
 
 const InvestorCommunityCard = () => {
   const investorTeam = [investor1, investor2, investor3];
   return (
-    <div className="w-full items-center rounded-3xl bg-gray-900">
-      <h2 className="mb-8 text-xl font-semibold text-white">Investors Entrepreneur Community</h2>
-      <div className="flex w-full">
-        {/* Main */}
-        <div className="mb-4 mr-24 flex overflow-hidden rounded-2xl">
-          <img src={mainImage} alt="Woman working at desk" className="h-auto object-cover" />
-          <button className="flex items-center space-x-2 rounded-full bg-gray-800 bg-opacity-80 px-4 py-2 text-white">
-            <span>Investors</span>
-            <ArrowRightIcon className="h-4 w-4" />
-          </button>
+    <div className="space-y-9 rounded-[20px] bg-accent p-9">
+      <h3 className="text-[20px] font-semibold text-white">Investors Entrepreneur Community</h3>
+      <div className="flex items-end gap-3">
+        <div className="mb-32 w-[40%]">
+          <div className="relative aspect-square w-full">
+            <img src={mainImage} alt="" className="h-full w-full rounded-[20px]" />
+            <button className="absolute bottom-[21px] right-[11px] flex items-center gap-2 rounded-full bg-background px-3 py-1 text-xs text-white">
+              Investors
+              <ArrowRightIcon className="h-2 w-2" />
+            </button>
+          </div>
         </div>
-
-        <div>
-          <div className="rounded-2xl bg-gray-800 p-4">
+        <div className="w-full">
+          <div className="w-[80%] rounded-[20px] bg-[#202541] p-4">
             <p className="mb-2 text-lg font-semibold text-white">
               Around <span className="text-blue-400">1,000</span> of investors teams.
             </p>
@@ -37,25 +38,22 @@ const InvestorCommunityCard = () => {
             </div>
             <div className="flex items-center justify-between">
               <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-700">
-                <GlobeIcon className="h-6 w-6 text-gray-400" />
+                <img src={GlobeIcon} alt="GlobeIcon" className="h-6 w-6 text-gray-400" />
               </div>
-              <button className="flex items-center space-x-1 text-blue-400">
+              <button className="flex items-center gap-2 rounded-full bg-background px-3 py-1 text-xs text-white">
                 <span>Know more</span>
                 <ArrowRightIcon className="h-4 w-4" />
               </button>
             </div>
           </div>
-
-          <div className="mt-2 flex items-start justify-around space-x-2">
-            <div className="w-[40%] overflow-hidden rounded-2xl">
-              <img src={handshake} alt="Handshake" className="h-full w-full object-cover" />
+          <div className="mt-[9px] grid w-full grid-cols-2 gap-4">
+            <div className="h-auto w-full">
+              <img src={handshake} alt="Handshake" className="h rounded-[20px]" />
             </div>
-            <div className="w-[60%]">
-              <button className="flex items-center space-x-2 rounded-full bg-blue-600 px-4 py-2">
+            <div className="h-auto w-full">
+              <button className="flex items-center gap-2 rounded-full bg-background px-3 py-1 text-xs text-white">
                 <span className="text-white">Global Investors around</span>
-                <div className="flex h-6 w-6 items-center justify-center rounded-full bg-blue-500">
-                  <PlusIcon className="h-4 w-4 text-white" />
-                </div>
+                <PlusIcon className="h-4 w-4 rounded-full bg-[#0A2F41] p-1 text-white" />
               </button>
             </div>
           </div>
--- src/pages/teams/teams.tsx
+++ src/pages/teams/teams.tsx
@@ -2,7 +2,7 @@ import InvestorCommunityCard from "./investorCommunityCard";
 
 const Teams = () => {
   return (
-    <div>
+    <div className="grid gap-[60px] lg:grid-cols-2">
       <InvestorCommunityCard />
     </div>
   );
