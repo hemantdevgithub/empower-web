ef83f6553285f1569886da1db1838295d29ecf37
imrannaazir <imrannaaziremon@gmail.com>
2024-09-28 15:33:55 +0600

    DISCORDED

--- src/routes/common.routes.tsx
+++ src/routes/common.routes.tsx
@@ -1,31 +1,16 @@
 import AddLanding from "@/pages/add/add-landing";
 import ReferPage from "@/pages/refer/refer-page";
 import SearchLanding from "@/pages/search/search-landing";
-import JobSeekers from "@/pages/search/service-providers/job-seekers";
-import { FaRegCircle } from "react-icons/fa";
 import { HiOutlineMagnifyingGlass } from "react-icons/hi2";
 import { IoMdAdd } from "react-icons/io";
 import { LiaShareAltSolid } from "react-icons/lia";
-import { PiSuitcaseSimple } from "react-icons/pi";
 
 export const commonPaths = [
   {
     name: "Search",
+    path: "search",
     icon: HiOutlineMagnifyingGlass,
-    children: [
-      {
-        element: <SearchLanding />,
-        path: "search",
-        name: "All",
-        icon: FaRegCircle,
-      },
-      {
-        name: "Job Seekers",
-        path: "search/job-seekers",
-        element: <JobSeekers />,
-        icon: PiSuitcaseSimple,
-      },
-    ],
+    element: <SearchLanding />,
   },
   {
     name: "Add",
